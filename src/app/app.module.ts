43f72f41c7d0c6c7e57023de51a3d839915efcdd
Ajitesh Shukla <ajitesh@gmail.com>
2017-11-05 10:29:18 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
